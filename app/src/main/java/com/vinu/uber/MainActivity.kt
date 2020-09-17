288291c0813671992d57efc110075c56a47feb4c
Vinukshen <vinukshen@gmail.com>
2020-09-17 09:48:22 +0100

    initial commit

---
+++ app/src/main/java/com/vinu/uber/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.vinu.uber
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide() //full screen
+    }
+}
